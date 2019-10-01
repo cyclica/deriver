b09298e14081ce60942860870acd6ef772a7348d
atomoton <andrew@brereton.me>
2019-10-01 10:40:44 -0400

    import the selfies alphabet from selfies directly

--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@ from setuptools import find_packages
 setup(name='deriver',
       description='Deriver: for all your molecule generation needs.',
       long_description='A software tool for the generation of novel chemical entities.',
-      version='1.0.0',
+      version='1.0.1',
       url='https://github.com/cyclica/deriver',
       license='All Rights Reserved Cyclica Inc.',
       packages=find_packages('src'),
       package_dir={'': 'src'},
       zip_safe=False,
-      install_requires=["selfies>=0.2.2"]
+      install_requires=["selfies>=0.2.4"]
       )
