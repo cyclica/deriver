1c216c3ebb241181606841821d4ac8563a041575
atomoton <andrew@brereton.me>
2019-12-09 14:21:00 -0500

    first version of random selfies generation

--- src/deriver/selfies_methods.py
+++ src/deriver/selfies_methods.py
@@ -7,6 +7,25 @@ import logging as logger
 
 ALPHABET = [symbol.strip("[]") for symbol in selfies_alphabet()]
 
+# follow these rules for which types of substitution should be allowed
+ALLOWED_SUBS = {
+    "C": ["N", "O", "H"],
+    "=C": ["=N", "N", "O", "=O", "S"],
+    "F": ["Cl", "Br", "I", "O", "N", "C", "H"],
+    "Cl": ["F", "Br", "I", "O", "N", "C", "H"],
+    "Br": ["Cl", "F", "I", "O", "N", "C", "H"],
+    "I": ["Cl", "Br", "F", "O", "N", "C", "H"],
+    "H": ["C", "O", "N", "S", "=C", "=O", "=S"],
+    "O": ["S", "N", "=O", "=N", "C", "=C", "Cl", "F", "Br", "I", "H"],
+    "=O": ["=S", "=N", "=C", "O"],
+    "N": ["O", "C", "H"],
+    "=N": ["=O", "O", "S", "=C", "C"],
+    "#N": ["#C"],
+    "S": ["O", "N", "C", "=O", "=N", "H"],
+    "=S": ["=O", "=N", "=C", "O"],
+    "#C": ["#N"]
+}
+
 
 def selfies_substitution(*,
                          parent_smiles: str,
@@ -38,25 +57,6 @@ def selfies_substitution(*,
     parent_selfies = encoder(parent_smiles)
     symbols = re.findall(r"[^[]*\[([^]]*)\]", parent_selfies)   # get the SELFIES symbols into a list
 
-    # follow these rules for which types of substitution should be allowed
-    allowed_subs = {
-        "C": ["N", "O", "H"],
-        "=C": ["=N", "N", "O", "=O", "S"],
-        "F": ["Cl", "Br", "I", "O", "N", "C", "H"],
-        "Cl": ["F", "Br", "I", "O", "N", "C", "H"],
-        "Br": ["Cl", "F", "I", "O", "N", "C", "H"],
-        "I": ["Cl", "Br", "F", "O", "N", "C", "H"],
-        "H": ["C", "O", "N", "S", "=C", "=O", "=S"],
-        "O": ["S", "N", "=O", "=N", "C", "=C", "Cl", "F", "Br", "I", "H"],
-        "=O": ["=S", "=N", "=C", "O"],
-        "N": ["O", "C", "H"],
-        "=N": ["=O", "O", "S", "=C", "C"],
-        "#N": ["#C"],
-        "S": ["O", "N", "C", "=O", "=N", "H"],
-        "=S": ["=O", "=N", "=C", "O"],
-        "#C": ["#N"]
-    }
-
     while spawns < n_children:  # try to produce the correct number of children
         muts = 0
         mutations = []  # which parts of the SELFIES to remove
@@ -70,7 +70,7 @@ def selfies_substitution(*,
                     if random.random() <= mut_rate:
                         # ignore special symbols, leave them alone
                         if not (symbols[pos] == "epsilon" or "Branch" in symbols[pos] or "Ring" in symbols[pos]):
-                            if symbols[pos] in allowed_subs:  # if we have rules for how to handle this symbol
+                            if symbols[pos] in ALLOWED_SUBS:  # if we have rules for how to handle this symbol
                                 mutations.append(pos)  # record which symbol this is
                                 muts += 1  # record the intention to mutate
                                 if muts == mut_max:
@@ -79,7 +79,7 @@ def selfies_substitution(*,
 
         # for each planned mutation, actually execute it, on the non-shuffled original SELFIES list
         for index in sorted(mutations, reverse=True):
-            mut_symbols[index] = random.choice(allowed_subs[mut_symbols[index]])
+            mut_symbols[index] = random.choice(ALLOWED_SUBS[mut_symbols[index]])
 
         # convert the new child into a SELFIES (rather than a list)
         child_symbols = [f"[{symb}]" for symb in mut_symbols]
@@ -107,11 +107,6 @@ def selfies_substitution(*,
         spawns += 1  # update our counter
 
     return children
-
-
-def random_selfies_generation(*, n_symbols: int = 50):
-    # should be a generator
-    yield NotImplementedError
 
 
 def selfies_insertion(*,
@@ -271,3 +266,22 @@ def selfies_deletion(*,
         spawns += 1  # update our counter
 
     return children
+
+
+def random_selfies_generator(*, n_symbols: int = 100):
+
+    rand_gen_alphabet = (list(ALLOWED_SUBS.keys()) * 2) + (["C"] * 50) + ALPHABET
+
+    while True:
+        symbols = [random.choice(rand_gen_alphabet) for i in range(n_symbols)]
+        # print(symbols)
+        # convert the new child into a SELFIES (rather than a list)
+        child_symbols = [f"[{symb}]" for symb in symbols]
+        child = "".join(child_symbols)
+        child_smiles = decoder(child)  # get the smiles
+        if len(child_smiles) < 10:
+            continue
+        elif Chem.MolFromSmiles(child_smiles) is None:
+            continue
+        else:
+            yield child_smiles
