82d5e22504e46481f5c3e170aed6ec8cbff2286c
atomoton <andrew@brereton.me>
2021-03-04 10:41:16 -0500

    add to reqs and delete bad test script

--- setup.py
+++ setup.py
@@ -4,12 +4,13 @@ from setuptools import find_packages
 setup(name='deriver',
       description='Deriver: for all your molecule generation needs.',
       long_description='A software tool for the generation of novel chemical entities.',
-      version='2.7.0',
+      version='2.7.1',
       url='https://github.com/cyclica/deriver',
       license='All Rights Reserved Cyclica Inc.',
       packages=find_packages('src'),
       package_dir={'': 'src'},
       include_package_data=True,
       zip_safe=False,
-      install_requires=["selfies==1.0.1", "peewee>=3.9.3", "numpy>=1.16.3", "crem==0.2.4", "loguru==0.4.1"]
+      install_requires=["selfies==1.0.1", "peewee>=3.9.3", "numpy>=1.16.3",
+                        "crem==0.2.4", "loguru==0.4.1", "pandas>=1.0.*"]
       )
