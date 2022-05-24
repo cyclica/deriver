5e3ba8183738d74c497a8e9d9b39ef4d40a7dd97
Joshua Sacher <joshuasacher@gmail.com>
2022-05-24 12:48:56 -0500

    add `num_heavy_atoms` and `fSP3` to `get_filter_values`, simplification of `get_filter_values` and `apply_filter`

--- src/deriver/child_filter.py
+++ src/deriver/child_filter.py
@@ -4,41 +4,18 @@ from rdkit.Chem import Crippen as crip
 from rdkit.Chem import Lipinski as lip
 from rdkit.Chem import rdMolDescriptors as desc
 from rdkit.Chem import rdmolops
+from rdkit.Chem import rdqueries
 from typing import List
 import pandas as pd
 import os
 import copy
 
-alerts = pd.read_csv(os.path.join(os.path.dirname(__file__), 'data/alert_collection.csv'))
-pains = alerts.loc[alerts['rule_set_name'] == 'PAINS', 'smarts']
-glaxo = alerts.loc[alerts['rule_set_name'] == 'Glaxo', 'smarts']
-always_filter = {'PAINS': pains, 'glaxo': glaxo}
-
-
-def get_atom_props(mol):
-    """
-    this function takes a mol and calculates the number of carbons, charges, and the size of the largest ring
-    """
-
-    num_carbons = 0
-    num_charges = 0
-    max_ring_size = 0
-
-    # loop over all the atoms
-    for at in mol.GetAtoms():
-        if at.GetSymbol() == "C":
-            num_carbons += 1
-        if at.GetFormalCharge() != 0:
-            num_charges += abs(at.GetFormalCharge())
-
-        # I suspect that there will never be a ring larger than 100, but this is technically an arbitrary cutoff
-        # this really could be optimized, but it's not rate-limiting enough to bother right now
-        for size in range(3, 101):
-            if at.IsInRingSize(size):
-                # which is larger, lazy method
-                max_ring_size = max((size, max_ring_size))
-
-    return num_carbons, num_charges, max_ring_size
+alerts = pd.read_csv(
+    os.path.join(os.path.dirname(__file__), "data/alert_collection.csv")
+)
+pains = alerts.loc[alerts["rule_set_name"] == "PAINS", "smarts"]
+glaxo = alerts.loc[alerts["rule_set_name"] == "Glaxo", "smarts"]
+always_filter = {"PAINS": pains, "glaxo": glaxo}
 
 
 def get_filter_values(mol):
@@ -49,6 +26,8 @@ def get_filter_values(mol):
 
     assert isinstance(mol, Chem.Mol)
 
+    atoms = mol.GetAtoms()
+
     values = {}
     values["MW"] = desc.CalcExactMolWt(mol)
     values["logP"] = crip.MolLogP(mol)
@@ -56,28 +35,46 @@ def get_filter_values(mol):
     values["HBD"] = lip.NumHDonors(mol)
     values["tPSA"] = desc.CalcTPSA(mol)
     values["rot_bonds"] = lip.NumRotatableBonds(mol)
-    values["rigid_bonds"] = mol.GetNumBonds() - values["rot_bonds"]  # assume mutual exclusion
+    # assume mutual exclusion
+    values["rigid_bonds"] = mol.GetNumBonds() - values["rot_bonds"]
     values["num_rings"] = lip.RingCount(mol)
+    values["max_ring_size"] = max(
+        (len(r) for r in mol.GetRingInfo().AtomRings()), default=0
+    )
+    values["num_carbons"] = len(
+        mol.GetAtomsMatchingQuery(rdqueries.AtomNumEqualsQueryAtom(6))
+    )
+    values["num_heavy_atoms"] = desc.CalcNumHeavyAtoms(mol)
     values["num_hetero_atoms"] = lip.NumHeteroatoms(mol)
-    values["charge"] = rdmolops.GetFormalCharge(mol)  # trusting this charge calculation method
-    values["num_carbons"], values["num_charges"], values["max_ring_size"] = get_atom_props(mol)
+    values["charge"] = rdmolops.GetFormalCharge(mol)
+    values["num_charges"] = sum((abs(atom.GetFormalCharge()) for atom in atoms))
     try:
-        values["hc_ratio"] = float(values["num_hetero_atoms"]) / float(values["num_carbons"])
+        values["hc_ratio"] = float(values["num_hetero_atoms"]) / float(
+            values["num_carbons"]
+        )
     except ZeroDivisionError:
         values["hc_ratio"] = 100000000  # if there are zero carbons
-    values["fc"] = len(list(Brics.FindBRICSBonds(mol)))  # how many BRICS bonds, related to complexity
-    values["is_good"] = True  # default to true, but not yet observed
-    atoms = [atom.GetSymbol() for atom in mol.GetAtoms()]  # get all the atoms, and make the list unique (only types)
-    atoms = set(atoms)
-    atoms = list(atoms)
-    values["atoms"] = atoms
-    values["num_chiral_centers"] = len(Chem.FindMolChiralCenters(mol, includeUnassigned=True))
+    # how many BRICS bonds, related to complexity
+    values["fc"] = len(list(Brics.FindBRICSBonds(mol)))
+    # get all the atoms, and make the list unique (only types)
+    values["atoms"] = list({atom.GetSymbol() for atom in atoms})
+    values["num_chiral_centers"] = len(
+        Chem.FindMolChiralCenters(mol, includeUnassigned=True)
+    )
+    values["fSP3"] = desc.CalcFractionCSP3(mol)
+    # default to true, but not yet observed
+    values["is_good"] = True
     values["rejections"] = []  # empty list to store the reasons for rejection
 
     return values
 
 
-def apply_filter(final_limits, children, must_have_patterns: List[str] = None, must_not_have_patterns: List[str] = None):
+def apply_filter(
+    final_limits,
+    children,
+    must_have_patterns: List[str] = None,
+    must_not_have_patterns: List[str] = None,
+):
 
     """
     Apply the filters to a set of molecules.
@@ -97,7 +94,7 @@ def apply_filter(final_limits, children, must_have_patterns: List[str] = None, m
     # compute the values to check for all the children
     child_values = {}
     for child in children:
-        #TODO: this is wasteful when it's already precalculated?
+        # TODO: this is wasteful when it's already precalculated?
         smile = Chem.MolToSmiles(child, isomericSmiles=True)
         # avoid filtering the same smile multiple times
         if smile in child_values:
@@ -114,40 +111,43 @@ def apply_filter(final_limits, children, must_have_patterns: List[str] = None, m
     # record each reason why a child violates the filter rules
     for smile in child_values:
         # ranges
-        if child_values[smile]["MW"] <= final_limits["MW"][0]:
+        if (
+            child_values[smile]["MW"] <= final_limits["MW"][0]
+            or child_values[smile]["MW"] >= final_limits["MW"][1]
+        ):
             child_values[smile]["is_good"] = False
             child_values[smile]["rejections"].append("MW")
-        if child_values[smile]["MW"] >= final_limits["MW"][1]:
-            child_values[smile]["is_good"] = False
-            child_values[smile]["rejections"].append("MW")
-        if child_values[smile]["num_carbons"] <= final_limits["num_carbons"][0]:
-            child_values[smile]["is_good"] = False
-            child_values[smile]["rejections"].append("num_carbons")
-        if child_values[smile]["num_carbons"] >= final_limits["num_carbons"][1]:
+        if (
+            child_values[smile]["num_carbons"] <= final_limits["num_carbons"][0]
+            or child_values[smile]["num_carbons"] >= final_limits["num_carbons"][1]
+        ):
             child_values[smile]["is_good"] = False
             child_values[smile]["rejections"].append("num_carbons")
-        if child_values[smile]["num_hetero_atoms"] <= final_limits["num_hetero_atoms"][0]:
+        if (
+            child_values[smile]["num_hetero_atoms"]
+            <= final_limits["num_hetero_atoms"][0]
+        ) or (
+            child_values[smile]["num_hetero_atoms"]
+            >= final_limits["num_hetero_atoms"][1]
+        ):
             child_values[smile]["is_good"] = False
             child_values[smile]["rejections"].append("num_hetero_atoms")
-        if child_values[smile]["num_hetero_atoms"] >= final_limits["num_hetero_atoms"][1]:
-            child_values[smile]["is_good"] = False
-            child_values[smile]["rejections"].append("num_hetero_atoms")
-        if child_values[smile]["hc_ratio"] <= final_limits["hc_ratio"][0]:
-            child_values[smile]["is_good"] = False
-            child_values[smile]["rejections"].append("hc_ratio")
-        if child_values[smile]["hc_ratio"] >= final_limits["hc_ratio"][1]:
+        if (
+            child_values[smile]["hc_ratio"] <= final_limits["hc_ratio"][0]
+            or child_values[smile]["hc_ratio"] >= final_limits["hc_ratio"][1]
+        ):
             child_values[smile]["is_good"] = False
             child_values[smile]["rejections"].append("hc_ratio")
-        if child_values[smile]["charge"] <= final_limits["charge"][0]:
+        if (
+            child_values[smile]["charge"] <= final_limits["charge"][0]
+            or child_values[smile]["charge"] >= final_limits["charge"][1]
+        ):
             child_values[smile]["is_good"] = False
             child_values[smile]["rejections"].append("charge")
-        if child_values[smile]["charge"] >= final_limits["charge"][1]:
-            child_values[smile]["is_good"] = False
-            child_values[smile]["rejections"].append("charge")
-        if child_values[smile]["logP"] <= final_limits["logP"][0]:
-            child_values[smile]["is_good"] = False
-            child_values[smile]["rejections"].append("logP")
-        if child_values[smile]["logP"] >= final_limits["logP"][1]:
+        if (
+            child_values[smile]["logP"] <= final_limits["logP"][0]
+            or child_values[smile]["logP"] >= final_limits["logP"][1]
+        ):
             child_values[smile]["is_good"] = False
             child_values[smile]["rejections"].append("logP")
 
@@ -176,13 +176,16 @@ def apply_filter(final_limits, children, must_have_patterns: List[str] = None, m
         if child_values[smile]["num_charges"] >= final_limits["num_charges"]:
             child_values[smile]["is_good"] = False
             child_values[smile]["rejections"].append("num_charges")
-        if child_values[smile]["num_chiral_centers"] >= final_limits["num_chiral_centers"]:
+        if (
+            child_values[smile]["num_chiral_centers"]
+            >= final_limits["num_chiral_centers"]
+        ):
             child_values[smile]["is_good"] = False
             child_values[smile]["rejections"].append("num_chiral_centers")
 
         # exclude smarts
         smarts_dict = copy.deepcopy(always_filter)
-        smarts_dict.update({'user_excluded': must_not_have_patterns})
+        smarts_dict.update({"user_excluded": must_not_have_patterns})
         for name in smarts_dict.keys():
             if filter_smarts(smile=smile, patterns=smarts_dict[name]):
                 child_values[smile]["is_good"] = False
