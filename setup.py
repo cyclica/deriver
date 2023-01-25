04c1b79c37c484e3715b423404b17f920893cd45
sconstable <steve.constable@cyclicarx.com>
2023-01-25 15:12:59 -0500

    loosen restrictions on loguru version to allow us to update

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages
 setup(name='deriver',
       description='Deriver: for all your molecule generation needs.',
       long_description='A software tool for the generation of novel chemical entities.',
-      version='2.7.3',
+      version='2.7.4',
       url='https://github.com/cyclica/deriver',
       license='All Rights Reserved Cyclica Inc.',
       packages=find_packages('src'),
@@ -12,5 +12,5 @@ setup(name='deriver',
       include_package_data=True,
       zip_safe=False,
       install_requires=["selfies==1.0.1", "peewee>=3.9.3", "numpy>=1.16.3",
-                        "crem==0.2.4", "loguru==0.4.1", "pandas>=1.0.*"]
+                        "crem==0.2.4", "loguru>=0.4.1", "pandas>=1.0.*"]
       )
